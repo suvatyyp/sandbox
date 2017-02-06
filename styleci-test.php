181f5848aad63fe45ebbfed02bcb68c5b8596f8f
Kurt <kurt@pixel.ee>
2017-02-06 22:45:43 +0200

    Create php file for StyleCI fixing

---
+++ styleci-test.php
@@ -0,0 +1,16 @@
+<?php
+
+class BadlyStyled
+{
+public function foo()
+{
+}
+      public function bar() {
+   $snake_var = 1;
+   $camelVar = 2; return $snake_var + $camelVar;
+}
+
+
+
+public function baz() { echo 'allononeline'    ; }    
+}
